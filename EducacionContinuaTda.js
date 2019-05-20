3ecba3fd7f954f5d7b66dea0d1b89e711fc18e00
elkingutierrex <elkin.gutierrex@gmail.com>
2019-05-19 21:51:46 -0500

    Genera matricula y archivo txt

--- EducacionContinuaTda.js
+++ EducacionContinuaTda.js
@@ -1,57 +1,50 @@
- const cursos = [
-	 {id : 1
-	 ,nombre:'fundamentos nodejs'
-	 ,duracionhoras:36
-	 ,valor:356000
-	 },
-	 {id : 2
-	 ,nombre:'programacion funcional'
-	 ,duracionhoras:25
-	 ,valor:488000
-	 },
-	 {id : 3
-	 ,nombre:'metodologia scrum'
-	 ,duracionhoras:12
-	 ,valor:156000
-	 },
-	 {id : 4
-	 ,nombre:'javascript avanzado1'
-	 ,duracionhoras:40
-	 ,valor:444000
-	 },
-	 {id : 5
-	 ,nombre:'javascriptavanzado2'
-	 ,duracionhoras:36
-	 ,valor:427000
-	 }
- ];		
+ const cursos = [{
+ 		id: 1,
+ 		nombre: 'fundamentos nodejs',
+ 		duracionhoras: 36,
+ 		valor: 356000
+ 	},
+ 	{
+ 		id: 2,
+ 		nombre: 'programacion funcional',
+ 		duracionhoras: 25,
+ 		valor: 488000
+ 	},
+ 	{
+ 		id: 3,
+ 		nombre: 'metodologia scrum',
+ 		duracionhoras: 12,
+ 		valor: 156000
+ 	},
+ 	{
+ 		id: 4,
+ 		nombre: 'javascript avanzado1',
+ 		duracionhoras: 40,
+ 		valor: 444000
+ 	},
+ 	{
+ 		id: 5,
+ 		nombre: 'javascriptavanzado2',
+ 		duracionhoras: 36,
+ 		valor: 427000
+ 	}
+ ];
+
+ 
+
+ let viewCursos = () => {
+	console.log('Bienvenido al tecnologio de Antioquia, los cursos ofertados son:')
+ 	cursos.forEach((curso) => {
+ 		setTimeout(() => {
+ 			console.log(curso)
+ 		},2000)
+ 	});
+ };
 
 
+ module.exports = {
+	 viewCursos
+	 ,cursos
+ }
 
-let viewCursos = (callback)=> {
-	cursos.forEach((curso)=> {
-		setTimeout(()=>{
-			callback(curso)
-		},2000)
-	});	
-};
-
-
-viewCursos(function(curso){
-	console.log(curso)
-})
-
-module.exports={
-	viewCursos	
-}
-
-
-
-
-// console.log(
-// 'BIENVENIDO, a los cursos de educación continua del TECNOLOGICO DE ANTIOQUIA'
-// +'' +
-// 'los cursos ofertados a la fecha' + '' + 'son: '
-// +'' + 
-
-// )	  
+ viewCursos()
--- opcionMatriculaTda.js
+++ opcionMatriculaTda.js
@@ -1,7 +1,93 @@
-const opcCurso = require ('./EducacionContinuaTda');
+const opcCurso = require('./EducacionContinuaTda');
+const fs = require('fs');
+
+const opciones = {
+    matricula: {
+        default: 0,
+        alias: 'm'
+    },
+    curso: {
+        default: 0,
+        alias: 'i'
+    },
+    nombre: {
+        default: 0,
+        alias: 'n',
+    },
+    cedula: {
+        default: 0,
+        alias: 'c',
+    }
+
+};
+
+console.log('Si desea generar matricula digite: ' + "\n" +
+    '-m=1' + "\n" +
+    '-i=(EL ID DEL CURSO)' + "\n" +
+    '-n=(SU NOMBRE)' + "\n" +
+    '-cc=(SU CEDULA)' + "\n");
 
 
-console.log('Bienvenido al tecnologio de Antioquia, los cursos ofertados son:')
+const argv = require('yargs')
+    .command('generarMatricula', 'generar Matricula', opciones)
+    .argv
+
+let generarMatricula = (argv) => {
+    if (argv.m == 1) {
+
+        let cursoX;
+
+        opcCurso.cursos.forEach((x) => {
+            if (x.id = argv.i) {
+                cursoX = x
+            }
+        })
+
+        if (!cursoX) {
+            console.log('El ID del curso que ingresastes NO EXISTE');
+            return;
+        } else {
+            texto = ('El nombre del estudiante es: ' + argv.n + '\n' +
+                ' Con cédula de ciudadania # :' + argv.c + '\n' +
+                ' se ha matriculado en el curso con id  : (' + argv.i + ')-' + cursoX.nombre + '\n' +
+                ' Tiene una duracion de de : '+cursoX.duracionhoras +' horas' + '\n' +
+                ' valor a cancelar de la matricula : $ ' + cursoX.valor);
+            fs.writeFile(argv.n + argv.c + 'promedio2.txt', texto, (err) => {
+                if (err) throw (err);
+                console.log('Se ha creado el archivo, y la matricula se ha generado correctamente, bienvenido al curso de ' + cursoX.nombre )
+            })
+
+        }
 
 
-console.log(opcCurso.viewCursos);
+
+    } else {
+        console.log('Cuando decidas matricularte no te olvides que en el TDA siempre tenemos una educación Continua!')
+    }
+
+}
+
+generarMatricula(argv);
+
+// //	Destructuracion
+// let {idCurso, nombre, curso: { matematicas, ingles, programacion}}	= estudiante;
+
+
+
+// console.log('El Nombre del estudiante ganador es '+ nombre);
+// console.log('El promedio del estudiante es: ' + optenerPromedio(matematicas,ingles,programacion));
+// console.log('la edad del estudiante es: ' + anos);
+
+//crear archivo
+
+// let crearArchivo = (estudiante) => {
+// 	texto = ('El nombre del estudiante es ' + nombre +  '\n'
+// 			+ 'ha optenido un promedios de ' + optenerPromedio(matematicas,ingles,programacion));
+// 	fs.writeFile(nombre+'promedio2.txt', texto,(err)=> {
+// 		if(err) throw(err);
+// 		console.log('se ha creado el archivo')
+// 	})
+
+// } 
+
+// crearArchivo(estudiante);
